abf9b72c5007cbe5fb7102425a12df54ea800517
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-30 15:49:37 -0400

    Made it so you can jump to your doom
    Refactored player.gd to add separate wide and skinny collision shapes and logic to switch between them based on movement state. Increased player speed, added jump and fall animations, and improved gravity and velocity handling. Updated player.tscn to include new collision shapes and adjusted animation resources. Set collision layers/masks for platform, adjusted player and camera positions in world.tscn, and made a minor color tweak in project.godot.

--- Game Stuff/Code/player.gd
+++ Game Stuff/Code/player.gd
@@ -4,27 +4,64 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 @onready var touching_ground = $"Touching ground"
 
-var speed = 100
+@onready var wide_collision = $CollisionShape2D
+@onready var wide_stand_collision = $"Touching ground/CollisionShape2D"
+@onready var skinny_collision = $Idle_collision_shape
+@onready var skinny_stand_collision = $"Touching ground/Idle_collision_shape"
+
+
+var speed = 500
 var gravity = 10
 var dir
-
+var jump_height = 340
 
 func _ready() -> void:
 	ani.play("Idle")
+	turn_on_wide_collision()
 
 func _physics_process(delta: float) -> void:
+		
 	if Input.is_action_pressed("left"):
 		sprite.flip_h = false
 		if touching_ground.get_overlapping_bodies().size() != 0: ani.play("Run")
 		dir = -1
+		turn_on_wide_collision()
 	elif Input.is_action_pressed("right"):
 		sprite.flip_h = true
 		if touching_ground.get_overlapping_bodies().size() != 0: ani.play("Run")
 		dir = 1
+		turn_on_wide_collision()
 	else:
 		if touching_ground.get_overlapping_bodies().size() != 0: ani.play("Idle")
 		dir = 0
-	if touching_ground.get_overlapping_bodies().size() != 0: gravity = 0
-	else: gravity += 10
-	velocity = Vector2(dir*speed, gravity)
+		turn_on_skinny_collision()
+		
+	if touching_ground.get_overlapping_bodies().size() != 0: velocity.y = 0
+	else:
+		velocity.y += gravity
+
+		
+		
+	if touching_ground.get_overlapping_bodies().size() == 0: ani.play("Fall")
+	elif Input.is_action_pressed("up"):
+		ani.play("Jump")
+		velocity.y = -1*jump_height
+		turn_on_wide_collision()
+	
+	velocity.x = dir*speed
 	move_and_slide()
+	
+	if global_position.y >= 650.0: get_tree().reload_current_scene()
+
+
+func turn_on_wide_collision():
+	if !skinny_collision.disabled: skinny_collision.disabled = true
+	if !skinny_stand_collision.disabled:	skinny_stand_collision.disabled = true
+	if wide_collision.disabled: wide_collision.disabled = false
+	if wide_stand_collision.disabled: wide_stand_collision.disabled = false
+
+func turn_on_skinny_collision():
+	if skinny_collision.disabled: skinny_collision.disabled = false
+	if skinny_stand_collision.disabled:	skinny_stand_collision.disabled = false
+	if !wide_collision.disabled: wide_collision.disabled = true
+	if !wide_stand_collision.disabled: wide_stand_collision.disabled = true
